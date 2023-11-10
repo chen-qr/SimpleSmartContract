eeb6e87e5384f3b6231f7857331bf9ea042c7407
callen <callenchen@outlook.com>
2023-11-10 16:53:32 +0800

    add next token id

--- MyNFT.sol
+++ MyNFT.sol
@@ -10,6 +10,10 @@ contract MyNFT {
         string owner;       // Token拥有者
     }
 
-    // 为每个token创建一个Token ID
+    // 为每个token创建一个Token ID，通过Token ID来检索Token
     mapping(uint256 => Token) private tokens;
+
+    // Token ID缓存值，每次创建Token时，用该值作为新增Token的ID
+    // 1 作为第一个ID，每次创建token后，值会增加
+    uint256 nextTokenId = 1;
 }
