6367ab221972d2cef97cfeb75996067f046588fe
Alex <alex.chenqr@gmail.com>
2023-12-08 15:42:34 +0800

    update

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -1,35 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
-contract SimpleToken {
-  // 保存账户地址对应的代币Token余额
-  mapping(address => uint256) private balances;
-  // 保存代币Token的总供应量
-  uint256 public totalSupply;
-  // 保存代币Token的发行人
-  address private owner;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  constructor() {
-    owner = msg.sender;
-  }
+contract SimpleToken is ERC20 {
+
+  constructor() ERC20("SimpleToken", "ST") {}
 
   // 增发代币Token
   function mint(address recipient, uint256 amount) public {
-    // 权限检验，只有代币的发行人，才能进行增发代币Token
-    require(msg.sender == owner, "Only the owner can perform this action");
-    balances[recipient] += amount;
-    totalSupply += amount;
-  }
-
-  // 检查账户的余额
-  function balanceOf(address account) public view returns (uint256) {
-    return balances[account];
-  }
-
-  // 转账
-  function transfer(address recipient, uint256 amount) public {
-    require(amount <= balances[msg.sender], "Not enough balance.");
-    balances[msg.sender] -= amount;
-    balances[recipient] += amount;
+    _mint(recipient, amount);
   }
 }
